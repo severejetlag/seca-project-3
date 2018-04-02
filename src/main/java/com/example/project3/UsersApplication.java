4c56984633c0595eedeb83aac0c731cf944985fc
severejetlag <nhl1013@gmail.com>
2018-04-02 00:15:10 -0700

    UPdated wait scrip to wait for '/' path. Created basic response for '/' Feature test successfully failing

--- src/main/java/com/example/project3/UsersApplication.java
+++ src/main/java/com/example/project3/UsersApplication.java
@@ -3,8 +3,10 @@ package com.example.project3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class UsersApplication {
 
 	@RequestMapping("/")
@@ -14,4 +16,5 @@ public class UsersApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UsersApplication.class, args);
-	}}
+	}
+}
