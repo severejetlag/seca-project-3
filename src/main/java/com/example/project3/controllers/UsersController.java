f8a44865bd0bfd8626eca714e8216a73d7db8afd
severejetlag <nhl1013@gmail.com>
2018-04-02 00:42:27 -0700

    Updating UsersController for basic template to be able to run unit tests

--- src/main/java/com/example/project3/controllers/UsersController.java
+++ src/main/java/com/example/project3/controllers/UsersController.java
@@ -1,4 +1,21 @@
 package com.example.project3.controllers;
 
+import com.example.project3.models.User;
+import com.example.project3.repositories.UserRepository;
+import javassist.NotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@RestController
 public class UsersController {
+    @Autowired
+    private UserRepository userRepository;
+
+    
 }
