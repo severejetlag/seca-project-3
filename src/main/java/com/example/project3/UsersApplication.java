fba2d1979627498c48ef8e1fb8170ed6af96ac40
severejetlag <nhl1013@gmail.com>
2018-04-01 22:59:31 -0700

    Created models, controllers and repositories directories along with basic User.model

--- src/main/java/com/example/project3/UsersApplication.java
+++ src/main/java/com/example/project3/UsersApplication.java
@@ -2,11 +2,16 @@ package com.example.project3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class UsersApplication {
 
+	@RequestMapping("/")
+	public String home() {
+		return "some users";
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(UsersApplication.class, args);
-	}
-}
+	}}
