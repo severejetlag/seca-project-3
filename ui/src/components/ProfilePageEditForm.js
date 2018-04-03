17832281e5a12cf10f1e6edeeddfeb99eccde09b
severejetlag <nhl1013@gmail.com>
2018-04-03 11:33:46 -0700

    Created profile edit form that will be displayed on the profile page for the user to select and edit the information related to the profile

---
+++ ui/src/components/ProfilePageEditForm.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+
+class ProfilePageEditForm extends Component{
+  state={
+    updatedUserInfo:{
+      ...this.props.currentUser
+    }
+  }
+  render(){
+    return(
+      <div>
+        <form action="">
+
+        </form>
+      </div>
+    )
+  }
+}
+
+export default ProfilePageEditForm
