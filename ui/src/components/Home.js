e64dc5705e68b219d4530fbb6db6fc0b7a82fe1c
severejetlag <nhl1013@gmail.com>
2018-04-03 01:27:03 -0700

    Added redirect to home page once user has logged in successfully.

--- ui/src/components/Home.js
+++ ui/src/components/Home.js
@@ -1,14 +1,20 @@
 import React, {Component} from 'react'
 import Nav from './Nav'
 import UserLoginForm from './UserLoginForm'
-
+import {Redirect} from 'react-router-dom'
 class Home extends Component{
-
   render(){
+    const hasCurrentUser = Object.keys(this.props.currentUser).length !== 0
+
+    // Redirect from Stack Overflow
+    //https://stackoverflow.com/questions/43230194/how-to-use-redirect-in-the-new-react-router-dom-of-reactjs
+    if(hasCurrentUser){
+      return <Redirect to='/users'/>
+    }
     return(
       <div>
         <Nav/>
-        <UserLoginForm userLogin={this.props.userLogin}/> 
+        <UserLoginForm userLogin={this.props.userLogin}/>
       </div>
     )
   }
