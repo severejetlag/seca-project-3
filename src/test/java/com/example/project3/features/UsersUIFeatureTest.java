d34ca661d6c7dfc22f6b2d3892153b2a096c54cc
severejetlag <nhl1013@gmail.com>
2018-04-02 22:21:32 -0700

    Simple UI test to make sure that everything is hooked up properly and we are able to grab an element off of a google search page.

--- src/test/java/com/example/project3/features/UsersUIFeatureTest.java
+++ src/test/java/com/example/project3/features/UsersUIFeatureTest.java
@@ -6,6 +6,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -57,5 +59,12 @@ public class UsersUIFeatureTest {
 
         System.setProperty("selenide.browser", "Chrome");
 
+        open("http://www.google.com");
+
+        WebElement queryBox = $(By.name("q"));
+        queryBox.sendKeys("Kent Beck");
+        queryBox.submit();
+
+        $("body").shouldHave(text("extreme programming"));
     }
 }
