1699582795df457443c265e7a119967da2b1e787
severejetlag <nhl1013@gmail.com>
2018-04-02 23:12:04 -0700

    Imported form from previous project and updated to allow for user login information.

---
+++ ui/src/components/UserLoginForm.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+
+class UserLoginForm extends Component {
+  state = {
+    userInfo: {}
+  }
+
+  handleChange = (event) => {
+    const attributeToChange = event.target.name
+    const newValue = event.target.value
+
+    const updatedUserInfo = { ...this.state.userInfo }
+    updatedUserInfo[attributeToChange] = newValue
+    this.setState({ userInfo: updatedUserInfo })
+  }
+  handleSubmit = (event) => {
+    event.preventDefault()
+
+    this.props.createPost(this.state.userInfo)
+    this.setState({userInfo:{}})
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Create New Idea</h2>
+
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label htmlFor="userName">Username</label>
+            <input
+              name="userName"
+              type="text"
+              onChange={this.handleChange} />
+            </div>
+
+            {/* <div>
+              <label htmlFor="password">password</label>
+              <textarea
+                name="password"
+                onChange={this.handleChange} />
+              </div> */}
+
+              <div>
+                <input type="submit" value="Login"/>
+              </div>
+            </form>
+
+          </div>
+        )
+      }
+    }
+
+    export default UserLoginForm
