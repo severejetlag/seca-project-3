c46f48d19414d07964c92e4b1bebf275b981f860
severejetlag <nhl1013@gmail.com>
2018-04-03 09:58:51 -0700

    Added delete user function to UsersPage compoment to pass down to child compoments.

--- ui/src/components/UsersPage.js
+++ ui/src/components/UsersPage.js
@@ -17,6 +17,20 @@ class UsersPage extends Component{
       }
   }
 
+  deletePost = async (userId, index) => {
+    try {
+        await axios.delete(`/users/${userId}`)
+
+        const updatedUserList = [...this.state.users]
+        updatedUserList.splice(index, 1)
+        this.setState({users: updatedUserList})
+
+    } catch (error) {
+        console.log(`Error deleting Idea with ID of ${postId}`)
+        console.log(error)
+    }
+  }
+
   render(){
     return(
       <div>
